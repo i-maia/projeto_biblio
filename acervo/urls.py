7695955710382a364f5ba391a2a7f3afa555cab7
maia <i.maia@escolar.ifrn.edu.br>
2026-08-30 17:27:46 -0300

    Ajustando rotas

--- acervo/urls.py
+++ acervo/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import LivroListView, LivroDetailView, AutorListView, AutorDetailView
 
 urlpatterns = [
+    path('', LivroListView.as_view(), name='home'), 
     path('livros/', LivroListView.as_view(), name='livro_list'),
     path('livros/<int:pk>/', LivroDetailView.as_view(), name='livro_detail'),
     path('autores/', AutorListView.as_view(), name='autor_list'),
